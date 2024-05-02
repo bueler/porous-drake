04c88ecec72763aa09cc12748db74ad2767fa16f
Ed Bueler <elbueler@alaska.edu>
2024-05-02 14:21:53 -0800

    update code to match doc; runs very stably

--- py/uporous.py
+++ py/uporous.py
@@ -1,39 +1,46 @@
 from firedrake import *
 from firedrake.output import VTKFile
 
-m = 100              # resolution
-lx = 100.0           # width
-lz = 22.0            # height
+mx = 200             # resolution
+mz = 44              # resolution
+
+lx = 100.0           # width (m)
+lz = 22.0            # height (m)
 
 ## for multiple units with discontinuous k:
 k1, phi1 = 6.87e-12, 0.500  # CV
 if True:
+    # moderated alternative: k2, phi2 = 4.94e-14, 0.0324 # OB
     k2, phi2 = 4.94e-15, 0.0324 # OB
     k3, phi3 = 2.18e-13, 0.232  # FV
 else:  # flatten
     k2, phi2 = k1, phi1
     k3, phi3 = k1, phi1
+# note: k in m^2, phi dimensionless
 
 # defined parameters
-R = 8.314462618         # universal gas constant 
-T = 293.15              # dome and gas temperature (K)
-M = 0.018015            # molar mass of steam 
-c = (R * T) / M         # ratio from ideal gas law
-mu = 0.000043           # dynamic viscosity
-g = 9.8                 # acceleration of gravity (m s-2)
-patm = 101325.0         # atmospheric pressure (Pa)
+R = 8.314462618         # universal gas constant    (J kg-1 mol-1)
+T = 293.15              # dome and gas temperature  (K)
+M = 0.018015            # molar mass of steam       (kg mol-1)
+c = (R * T) / M         # ratio from ideal gas law  (J kg-1)
+mu = 0.000043           # dynamic viscosity         (Pa s)
+g = 9.8                 # acceleration of gravity   (m s-2)
 
 # indices of four boundaries/sides:
 #   (1, 2, 3, 4) = (left, right, bottom, top)
-mesh = RectangleMesh(m, m, lx, lz, quadrilateral=True)
+mesh = RectangleMesh(mx, mz, lx, lz, quadrilateral=True)
 
 # choose function spaces
-S = FunctionSpace(mesh, 'RTCF', 1)
-H = FunctionSpace(mesh, 'DG', 0)
-W = S * H
-w = Function(W)
-sigma, u = split(w)              # sigma = (mass flux), u = rho^2/2
-omega, v = TestFunctions(W)
+order = 0
+S = FunctionSpace(mesh, 'RTCF', order + 1)
+H = FunctionSpace(mesh, 'DG', order)
+# alternative which seems stable with order = 1:
+#S = FunctionSpace(mesh, 'RTCF', order + 1)
+#H = FunctionSpace(mesh, 'CG', order)
+SH = S * H
+w = Function(SH)
+sigma, u = split(w)              # sigma = (mass flux), u = rho^2
+omega, v = TestFunctions(SH)
 
 # permeability and porosity
 x, z = SpatialCoordinate(mesh)   # x horizontal, z vertical
@@ -44,34 +51,42 @@ k = conditional(z < 12.0, k1, conditional(abs(x - 50.0) < 4.0, k1, kupper))
 phiupper = conditional(z < 18.0, phi2, conditional(abs(x - 50.0) < 12.0, phi2, phi3))
 phi = conditional(z < 12.0, phi1, conditional(abs(x - 50.0) < 4.0, phi1, phiupper))
 
-# FIXME:  P0 = patm * exp((g/c) * (lz - z)
+# lithostatic pressure field
+Patm = 101325.0                  # atmospheric pressure (Pa)
+P0 = Patm * exp((g/c) * (lz - z))
 
-# steam density boundary conditions
-dens_top = patm / c # kg/m^3
-pressure_bottom = 1100000.0   # Pa; = 11 bar
-dens_bottom = pressure_bottom / c
+# Dirichlet boundary conditions
+verif = False
+u_top = (Patm / c)**2
+if verif:
+    Pbottom = P0
+else:
+    Pbottom = 1100000.0   # Pa; = 11 bar
+u_bottom = (Pbottom / c)**2
 
 # mixed weak form; see doc.pdf
 n = FacetNormal(mesh)
-G = (c + g * z) / mu
+alf, bet = c / (2.0 * mu), 1.0 / mu
 F = dot(sigma, omega) * dx \
-    + 2.0 * k * u * dot(grad(G), omega) * dx \
-    - u * div(k * G * omega) * dx \
-    + avg(u) * jump(k * omega, n) * dS \
+    - alf * u * div(k * omega) * dx \
+    - bet * k * sqrt(u) * dot(grad(P0), omega) * dx \
+    + alf * avg(u) * jump(k * omega, n) * dS \
     + div(sigma) * v * dx \
-    + (k / 2.0) * dens_bottom**2 * dot(omega, n) * ds(3) \
-    + (k / 2.0) * dens_top**2 * dot(omega, n) * ds(4)
+    + alf * k * u_bottom * dot(omega, n) * ds(3) \
+    + alf * k * u_top * dot(omega, n) * ds(4)
 
 # Neumann conditions on u for ids 1,2 is now Dirichlet on normal
 # component of sigma; we must set both components apparently
-BCs = DirichletBC(W.sub(0), as_vector([0.0,0.0]), (1,2))
+BCs = DirichletBC(SH.sub(0), as_vector([0.0,0.0]), (1,2))
 
 print('solving weak mixed form for sigma, u ...')
 sigma, u = w.subfunctions
-u.assign(dens_top**2/2)  # initial iterate nonzero (equals top b. c.)
+u.assign(u_top)  # initial iterate nonzero (equals top b. c.)
 sigma.assign(as_vector([0.0,0.0]))
-solve(F == 0, w, bcs=[BCs,], options_prefix='main',
-      solver_parameters = {'snes_type': 'ksponly',
+solve(F == 0, w, bcs=[BCs,], options_prefix='s',
+      solver_parameters = {'snes_type': 'newtonls',
+                           'snes_converged_reason': None,
+                           'snes_monitor': None,
                            'ksp_type': 'preonly',
                            'pc_type': 'lu',
                            'pc_factor_mat_solver_type': 'mumps'})
@@ -101,13 +116,25 @@ print(f'  FV unit flux            = {FVflux:13.6e} ({100*FVflux/topflux:5.2f} %)
 
 sigma, u = w.subfunctions
 sigma.rename('sigma (mass flux; kg m-2 s-1)')
-u.rename('u (rho^2/2)')
-rho = Function(H).interpolate(sqrt(2.0 * u))
-rho.rename('rho (density; kg m-3')
+u.rename('u (rho^2)')
+
+uneg = Function(H).interpolate((-u + abs(u))/2.0)
+uneg.rename('u_- (negative part of u)')
+if norm(uneg, 'L2') > 0.0:
+    print('WARNING: nonzero negative part of u(x,z) detected')
+    print('         see "uneg" field in output file')
+upos = Function(H).interpolate((u + abs(u))/2.0)
+
+rho = Function(H).interpolate(sqrt(abs(u)))
+rho.rename('rho (density; kg m-3)')
+P = Function(H).interpolate(c * rho)
+P.rename('p (pressure; Pa)')
+P0 = Function(H).interpolate(P0)
+P0.rename('P0 (lithostatic pressure; Pa)')
 q = Function(S).project(sigma / rho)  # interpolate throws error re dual basis
 q.rename('q (Darcy volumetric flux; m s-1)')
 v = Function(S).project(q / phi)      # ditto
 v.rename('v (velocity; m s-1)')
 
 print('saving fields to result.pvd ...')
-VTKFile("result.pvd").write(sigma, u, rho, q, v)
+VTKFile("result.pvd").write(sigma, u, uneg, rho, P, P0, q, v)
